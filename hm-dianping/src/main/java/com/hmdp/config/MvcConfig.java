405a286da4e9d8a199d93f847d5b05500c991692
wuhuqifei <1214086027@qq.com>
2023-05-28 08:53:18 +0800

    v1

---
+++ hm-dianping/src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,32 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author yongzh
+ * @version 1.0
+ * @program: hm-dianping
+ * @description:
+ * @date 2023/5/28 0:17
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/user/code",
+                        "/user/login",
+                        "/blog/hot",
+                        "/shop/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/voucher/**"
+                );
+
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+}
